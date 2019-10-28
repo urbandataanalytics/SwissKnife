cc327aaa80b322485b6b59ca00a542c606b4ed5e
Julián Sierra <julian.sierra@udarealestate.com>
2019-10-28 16:27:41 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(name='SwissKnife',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=[],
-     )
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Greg Ward',
+    author_email='gward@python.net',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=setuptools.find_packages()
+)
