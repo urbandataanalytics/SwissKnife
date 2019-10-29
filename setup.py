42aca95f09c68ca36140afc73c1f985077a413ac
Julián Sierra <julian.sierra@udarealestate.com>
2019-10-29 13:24:50 +0100

    added changleog

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-setuptools.setup(name='SwissKnife',
-    version='1.0',
-    description='Python Distribution Utilities',
-    author='Greg Ward',
-    author_email='gward@python.net',
-    url='https://www.python.org/sigs/distutils-sig/',
+setuptools.setup(
+    name='SwissKnife',
+    version='0.1',
+    description='Utils and common libraries for Python',
+    author='UDARealState Data engineering Team',
+    url='https://bitbucket.org/udanalytics/swissknife',
     packages=setuptools.find_packages()
 )
