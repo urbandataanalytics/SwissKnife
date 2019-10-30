2b0a06af5c4d5a589f5f5b65777c6dc9b4512838
Julián Sierra <julian.sierra@udarealestate.com>
2019-10-30 17:32:34 +0100

    documented SwissKnife

--- SwissKnife/info/ExecutionEnvironment.py
+++ SwissKnife/info/ExecutionEnvironment.py
@@ -2,10 +2,11 @@ from enum import Enum
 
 
 class ExecutionEnvironment(Enum):
-    """
-        Execution environment
-
-        Devuelve pro y esas cosas
+    """ExutionEnvironment is a Enum that represents a possible ExecutionEnvironment.
+    Valid values are PRO, PRE and DEV. PRE is the default value.
+    The static method "create" is the preferred method to create and ExecutionEnvironment because
+    it transform the input to an standard form (lower case) and return the default value (PRE)
+    if a invalid value (or a null value) is provided. 
     """
 
     PRO = "pro"
@@ -14,6 +15,15 @@ class ExecutionEnvironment(Enum):
 
     @staticmethod
     def create(env_str: str) -> "ExecutionEnvironment":
+        """Create an ExecutionEnvironment from the provided text. Convert it
+        convert it to "lower case" and match the result with one of the possibilities.
+        If there is not a match, PRE value will be the final result.
+        
+        :param env_str: Environment value in text format.
+        :type env_str: str
+        :return: An ExecutionEnvironment based on the previous value.
+        :rtype: ExecutionEnvironment
+        """
 
         # Is none or other invalid type.
         if type(env_str) is not str:
@@ -25,25 +35,27 @@ class ExecutionEnvironment(Enum):
             return ExecutionEnvironment("pre")
 
     def is_pro(self):
+        """Return if the value of this ExecutionEnvironment is PRO.
+        
+        :return: True if this ExecutionEnvironment is PRO.
+        :rtype: bool
         """
-            Return if the current execution environment is PRO.
-
-            Returns
-            -------
-            bool
-                True is this execution environment is PRO, False otherwise.
-        """
-
         return self == ExecutionEnvironment.PRO
 
     def is_pre(self):
+        """Return if the value of this ExecutionEnvironment is PRE.
+        
+        :return: True if this ExecutionEnvironment is PRE.
+        :rtype: bool
+        """
 
         return self == ExecutionEnvironment.PRE
 
     def is_dev(self):
+        """Return if the value of this ExecutionEnvironment is DEV.
+        
+        :return: True if this ExecutionEnvironment is DEV.
+        :rtype: bool
+        """
 
         return self == ExecutionEnvironment.DEV
-
-
-if __name__ == "__main__":
-    help(ExecutionEnvironment.is_pro)
