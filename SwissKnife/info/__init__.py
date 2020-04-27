440519d0a11e02620e817872c3f5b231bc5f5177
Juanlu <juan.luis.gomez@udarealestate.com>
2020-04-27 16:25:07 +0200

    Reduce if statement to one liner

--- SwissKnife/info/__init__.py
+++ SwissKnife/info/__init__.py
@@ -3,8 +3,4 @@ import os
 # With this, we can import directly the class without importing the module.
 from SwissKnife.info.ExecutionEnvironment import ExecutionEnvironment
 
-
-if "ENV" in os.environ:
-    CURRENT_ENVIRONMENT = ExecutionEnvironment.create(os.environ["ENV"])
-else:
-    CURRENT_ENVIRONMENT = ExecutionEnvironment.create(None)
+CURRENT_ENVIRONMENT = ExecutionEnvironment.create(os.environ.get('ENV', None))
