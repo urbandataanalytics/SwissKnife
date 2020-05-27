91efbd7fb553c0d890850952e873dc26fbb2741b
Javier Sanz <jsanz@udarealstate.com>
2020-05-27 18:17:52 +0200

    Fixed docstring and removed some comments.

--- SwissKnife/calendar/WeekUtils.py
+++ SwissKnife/calendar/WeekUtils.py
@@ -2,16 +2,14 @@ from datetime import datetime
 
 
 def get_week_format_from_timestamp(timestamp_millis: int) -> str:
-    """Get week format from timestamp in milliseconds.
+    """Get week format from timestamp in milliseconds. The week number
+    returned will follow ISO-8601 standard
 
-    :param timestamp_millis: Timestamp en milliseconds.
+    :param timestamp_millis: Timestamp in milliseconds.
     :type timestamp_millis: int
-    :return: Return in week format {year}W{week}.
+    :return: Return in week format {year}W{week}. Note that the week will be pad to two digits, e.g: 2020W01
     :rtype: str
     """
-
-    # This algorithm avoids results like 2019W01 for a 2019-12-31 date
-    # and 2021W53 for a 2021-01-01 date
 
     date_object = datetime.utcfromtimestamp(int(timestamp_millis) / 1000)
     calendar = date_object.isocalendar()
