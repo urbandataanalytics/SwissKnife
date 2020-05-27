63dc52be39da035b565fb96a66fe28d9774f4d86
Javier Sanz <jsanz@udarealstate.com>
2020-05-27 18:22:38 +0200

    Fixing tests comments.

--- tests/calendar/test_WeekUtils.py
+++ tests/calendar/test_WeekUtils.py
@@ -11,12 +11,12 @@ class TestWeekUtils(unittest.TestCase):
         self.assertEqual(result, "2017W52")
 
     def test_get_first_week_with_a_previous_year_day(self):
-        timestamp_millis = 1577709695547  # 2017-12-31 (Sunday)
+        timestamp_millis = 1577709695547  # 2019-12-30 (Sunday)
         result = get_week_format_from_timestamp(timestamp_millis)
         self.assertEqual(result, "2020W01")
 
     def test_get_last_week_with_a_next_year_day(self):
-        timestamp_millis = 1609459200000  # 20121-01-01 (Friday)
+        timestamp_millis = 1609459200000  # 2021-01-01 (Friday)
         result = get_week_format_from_timestamp(timestamp_millis)
         self.assertEqual(result, "2020W53")
 
