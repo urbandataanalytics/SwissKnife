23cb24f057a106daa88fcee2115d8617d0d342eb
Pablo Cabezas <pablo.cabezas@udarealestate.com>
2021-02-19 16:51:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DEPENDENCIES = [
 
 EXTRA_DEPENDENCIES = {
     "avro": ["fastavro==0.22.7"],
-    "gcloud": ["google-cloud-storage==1.23.0"],
+    "gcloud": ["google-cloud-storage==1.23.0", "backoff==1.10.0"],
     "all": ["fastavro==0.22.7", "google-cloud-storage==1.23.0"]
 }
 
@@ -16,7 +16,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='UDASwissKnife',
-    version='0.9.0',
+    version='0.10.0',
     description='Utils and common libraries for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
